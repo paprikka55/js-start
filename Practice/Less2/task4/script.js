a58ebae06ba54cf4ecddd6927ab9abed6d6a1d79
Alexander Malyatin <paprikka84@gmail.com>
2024-12-12 11:16:32 +0600

    Practice Less2

---
+++ Practice/Less2/task4/script.js
@@ -0,0 +1,30 @@
+/* Задание 4 (тайминг 15 минут)
+1. Не запуская код, определите, что выведется в консоль:
+a. console.log(String(true));
+2. Не запуская код, определите, что выведется в консоль:
+a. console.log('a' + true);
+3. Не запуская код, определите, что выведется в консоль:
+a. console.log(Number(true));
+4. Не запуская код, определите, что выведется в консоль:
+a. console.log(true + 1);
+5. Не запуская код, определите, что выведется в консоль:
+a. console.log(true + true)
+6. Не запуская код, определите, что выведется в консоль:
+a. console.log(true - true)
+7. Не запуская код, определите, что выведется в консоль:
+a. console.log( String(true) + Number(true) )*/
+
+// true
+console.log(String(true));
+// atrue
+console.log("a" + true);
+// 1
+console.log(Number(true));
+// 2
+console.log(true + 1);
+// 2
+console.log(true + true);
+// 0
+console.log(true - true);
+// true1
+console.log(String(true) + Number(true));
